322a7ad266af36c0f782c48a2c6ab2235cf0f92b
brendanfitz <brendanjfitzpatrick0@gmail.com>
2023-04-02 16:52:12 -0500

    Finish asl-signs 3d-viz plotly app

--- asl-signs/asl_3d/app.py
+++ asl-signs/asl_3d/app.py
@@ -8,7 +8,12 @@ import plotly.express as px
 
 LM_TYPES = ['right_hand', 'pose', 'left_hand']
 
-seq = LandmarkSequence('1000210073')
+seq = LandmarkSequence('1002052130')
+
+graph_style = dict(width='30%', display='inline-block')
+
+sign = 'TV'
+sequences = seq.SIGN_META.loc[seq.SIGN_META.sign == sign, ].index.tolist()
 
 figures = {
     lm_type: go.Figure(data=create_traces(seq, lm_type), layout=go.Layout(title=lm_type))
@@ -20,12 +25,81 @@ app = dash.Dash()
 app.layout = html.Div(children=[
     html.H1('Landmark Sequences', style=dict(textAlign='center')),
     html.Div(
+        id='sequence-selectors',
+        children=[
+            dcc.Dropdown(
+                id='sign-selector',
+                options=seq.SIGN_META.sign.unique(),
+                value=sign,
+            ),
+            dcc.Dropdown(
+                id='sequence-selector',
+                options=sequences,
+                value=sequences[0],
+            )
+        ]
+    ),
+    html.Div(
         id='graphs',
         children=[dcc.Graph(id=lm_type, figure=figures[lm_type], style=dict(width='30%', display='inline-block'))
                   for lm_type in LM_TYPES],
         style=dict(style='display:flex; flex-direction: row; justify-content: center; align-items: center')
     ),
-    dcc.Slider(
+    html.Div(
+        id='frame-div',
+        children=[
+            dcc.Slider(
+                id='frame-picker',
+                min=min(seq.frames),
+                max=max(seq.frames),
+                step=None,
+                marks={frame: str(frame) for frame in seq.frames},
+                value=min(seq.frames)
+            )
+        ]
+
+    )
+])
+
+@app.callback(Output('pose', 'figure'), [Input('frame-picker', 'value')])
+def update_pose(selected_frame):
+    lm_type = 'pose'
+    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
+    return fig
+
+@app.callback(Output('left_hand', 'figure'), [Input('frame-picker', 'value')])
+def update_left_hand(selected_frame):
+    lm_type = 'left_hand'
+    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
+    return fig
+        
+@app.callback(Output('right_hand', 'figure'), [Input('frame-picker', 'value')])
+def update_right_hand(selected_frame):
+    lm_type = 'right_hand'
+    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
+    return fig
+
+@app.callback(Output('sequence-selector', 'options'), [Input('sign-selector', 'value')])
+def update_sequence_options(sign):
+    sequences = seq.SIGN_META.loc[seq.SIGN_META.sign == sign, ].index.tolist()
+    return sequences
+
+@app.callback(Output('graphs', 'children'), [Input('sequence-selector', 'value')])
+def update_figs(sequence_id):
+    global seq
+    global figures
+    seq = LandmarkSequence(sequence_id)
+    figures = {
+        lm_type: go.Figure(data=create_traces(seq, lm_type), layout=go.Layout(title=lm_type))
+        for lm_type in LM_TYPES
+    }
+    children=[dcc.Graph(id=lm_type, figure=figures[lm_type], style=graph_style)
+              for lm_type in LM_TYPES]
+    return children
+
+@app.callback(Output('frame-div', 'children'), [Input('graphs', 'children')])
+def update_frames(_):
+    return dcc.Slider(
         id='frame-picker',
         min=min(seq.frames),
         max=max(seq.frames),
@@ -33,25 +107,8 @@ app.layout = html.Div(children=[
         marks={frame: str(frame) for frame in seq.frames},
         value=min(seq.frames)
     )
-])
 
-@app.callback(Output('pose', 'figure'), [Input('frame-picker', 'value')])
-def update_figure(selected_frame):
-    lm_type = 'pose'
-    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
-    return fig
-
-@app.callback(Output('left_hand', 'figure'), [Input('frame-picker', 'value')])
-def update_figure_lh(selected_frame):
-    lm_type = 'left_hand'
-    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
-    return fig
-        
-@app.callback(Output('right_hand', 'figure'), [Input('frame-picker', 'value')])
-def update_figure_lh(selected_frame):
-    lm_type = 'right_hand'
-    fig = change_visible_frame(lm_type, figures[lm_type], seq.frames, selected_frame)
-    return fig
+    
         
 
 if __name__ == '__main__':
